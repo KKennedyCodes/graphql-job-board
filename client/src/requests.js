ba69ef764a9851530f92cdbfcfa8f7a21f86c766
KKennedyCodes <holmstedtk@gmail.com>
2021-03-05 05:04:02 -0800

    Refactors graphql requests to use shared function and added graphql error handling

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,45 +1,46 @@
 const endpointURL = 'http://localhost:9000/graphql';
 
-export async function loadJobs() {
+async function graphQLRequest(query, variables={}){
   const response = await fetch(endpointURL, {
     method: 'POST',
     headers: {'content-type': 'application/json'},
-    body: JSON.stringify({
-      query: `{
-        jobs {
-          id
-          title
-          company {
-            id
-            name
-          }
-        }
-      }`
-    })
+    body: JSON.stringify({query,variables})
   });
   const responseBody = await response.json();
-  return responseBody.data.jobs;
+  if(responseBody.errors) {
+    const message = responseBody.errors.map((error) => error.message).join('\n');
+    throw new Error(`GraphQL Error: ${message}`);
+  }
+  return responseBody.data;
+}
+
+export async function loadJobs() {
+  const query = `{
+    jobs {
+      id
+      title
+      company {
+        id
+        name
+      }
+    }
+  }`
+  const {jobs} = await graphQLRequest(query)
+  return jobs;
 }
 
 export async function loadJob(id) {
-  const response = await fetch(endpointURL, {
-    method: 'POST',
-    headers: {'content-type': 'application/json'},
-    body: JSON.stringify({
-      query: `query JobQuery($id: ID!) {
-        job(id: $id) {
-          id
-          title
-          company {
-            id
-            name
-          }
-          description
-        }
-      }`,
-      variables: {id}
-    })
-  });
-  const responseBody = await response.json();
-  return responseBody.data.job;
+  const query = `query JobQuery($id: ID!) {
+    job(id: $id) {
+      id
+      title
+      company {
+        id
+        name
+      }
+      description
+    }
+  }`
+  const {job} = await graphQLRequest(query, {id})
+  return job;
 }
